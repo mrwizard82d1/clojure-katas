25592216dc0723d4b7c10120eb5b7407cdb79a26
Larry Jones <mrwizard82d1@gmail.com>
2018-03-17 15:57:40 -0500

    Sine angle approximation kata

--- src/clojure_katas/sine_angle.clj
+++ src/clojure_katas/sine_angle.clj
@@ -6,4 +6,10 @@
   by making use of the approximation sin x  x if x is sufficiently small,
   and the trigonometric identity:
     sin(r) = 3sin(r/3) -. 4sin^3(r/3)"
-  [angle])
+  [angle]
+  (let [cube (fn [x] (* x x x))
+        sine-3x (fn [x] (- (* 3 (sine x))
+                           (* 4 (cube (sine x)))))]
+    (if (<= (Math/abs angle) 0.1)
+      angle
+      (sine-3x (/ angle 3)))))
